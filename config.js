07443f9ba4b7b75f9f3f9bcbb60a17116f32bc9b
siddharth301 <74924743+siddharth301@users.noreply.github.com>
2021-09-20 19:16:57 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBI3MicA2mCy4ds6BP5QjORrYZGMt-JC0A",
+  authDomain: "book-story-telling-app-c3151.firebaseapp.com",
+  projectId: "book-story-telling-app-c3151",
+  storageBucket: "book-story-telling-app-c3151.appspot.com",
+  messagingSenderId: "941785326570",
+  appId: "1:941785326570:web:586a808b0c9eeb4b374893"
+};
